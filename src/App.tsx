eb1307617d2d11cedf103a95750b9f1b50b3e237
rogerio3001 <rogeriovinicios3001@gmail.com>
2022-10-02 08:28:37 -0300

    add: carrosel of months

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React/* , { useRef }  */from "react";
+import months from "./months";
 
 function App() {
+  // const carroselItem = useRef<HTMLDivElement>(null);
+  function handleClick(key: number) {
+    const element = document.getElementsByClassName("carrosel-item-" + key)[0];
+    element.scrollIntoView({
+      inline: "center",
+      behavior: "smooth",
+      block: "center",
+    });
+    // indo pro fim do scroll
+    // carroselItem.current?.scrollIntoView({
+    //   inline: "center",
+    //   behavior: "smooth"
+    // })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="carrosel-container">
+        {months.map((month, index) => {
+          return (
+            <div
+              // ref={carroselItem}
+              key={index}
+              className={"carrosel-item carrosel-item-" + index}
+              onClick={() => handleClick(index)}
+            >
+              {month}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
